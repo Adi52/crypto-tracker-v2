fa475b72b684d07bf7efa24b56ac10c57b595527
Adrian Bieliński <adimr52@gmail.com>
2021-12-05 13:06:07 +0100

    Create login page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,29 +1,43 @@
 import Head from "next/head";
-import { useSession, signIn, signOut } from "next-auth/react";
+import { useSession, signIn, signOut, getSession } from "next-auth/react";
+import Button from "../components/Button";
 
 export default function Home() {
   const { data: session } = useSession();
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-gray-700">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main>
-        <h1>CRYPTO TRACKER</h1>
-        {!session ? (
-          <>
-            Not Signed in <br />
-            <button onClick={signIn}>Sign In</button>
-          </>
-        ) : (
-          <>
-            Signed in as {session.user.email} <br />
-            <button onClick={signOut}>Sign Out</button>
-          </>
-        )}
-      </main>
+    <div>
+      <h1 className="text-white text-4xl md:text-8xl font-montserrat font-bold font-center">
+        CRYPTO TRACKER
+      </h1>
+      <div className="flex items-center justify-center p-8 mt-20">
+        <Button onClick={signIn} title={"Log In / Sign Up"} />
+      </div>
+      {/*{!session ? (*/}
+      {/*  <>*/}
+      {/*    Not Signed in <br />*/}
+      {/*    <button onClick={signIn}>Sign In</button>*/}
+      {/*  </>*/}
+      {/*) : (*/}
+      {/*  <>*/}
+      {/*    Signed in as {session.user.email} <br />*/}
+      {/*    <button onClick={signOut}>Sign Out</button>*/}
+      {/*  </>*/}
+      {/*)}*/}
     </div>
   );
+}
+
+export async function getServerSideProps(ctx) {
+  const session = await getSession(ctx);
+
+  if (session) {
+    return {
+      redirect: { destination: "/dashboard", permanent: true },
+    };
+  }
+
+  return {
+    props: {},
+  };
 }
