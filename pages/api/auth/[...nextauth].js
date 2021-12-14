db3eea6a863d112e41de496226fe184110bf48ad
Adrian Bieliński <adimr52@gmail.com>
2021-12-15 00:46:18 +0100

    Add google provider

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,16 +1,13 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import EmailProvider from "next-auth/providers/email";
+import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import prisma from "../../../prisma/prisma";
 
 const options = {
   adapter: PrismaAdapter(prisma),
   providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
-    }),
     EmailProvider({
       server: {
         host: process.env.EMAIL_SERVER_HOST,
@@ -22,7 +19,23 @@ const options = {
       },
       from: process.env.EMAIL_FROM,
     }),
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
+    }),
   ],
+  secret: process.env.SECRET,
   database: {
     type: "postgresql",
     database: "",
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,22 +1,46 @@
-import { useSession, signIn, signOut, getSession } from "next-auth/react";
-import Button from "../components/Button";
+import { useSession, signIn, getSession, getProviders } from "next-auth/react";
+import {
+  GithubLoginButton,
+  createButton,
+  GoogleLoginButton,
+} from "react-social-login-buttons";
 
-export default function Home() {
+const EmailLoginButton = createButton({
+  text: "Log in with e-mail",
+  icon: "mail",
+  style: { background: "white", color: "black" },
+  activeStyle: { background: "#dcdcdc" },
+});
+
+export default function Home({ providers }) {
   const { data: session } = useSession();
 
   return (
-    <div>
+    <div className="min-h-screen flex flex-col justify-center items-center">
       <h1 className="text-white text-4xl md:text-8xl font-montserrat font-bold font-center">
         CRYPTO TRACKER
       </h1>
-      <div className="flex items-center justify-center p-8 mt-20">
-        <Button
+      <div className="flex flex-col items-center justify-center p-8 mt-20 max-w-md w-full">
+        <GithubLoginButton
+          onClick={() =>
+            signIn(providers.github.id, {
+              callbackUrl: `${process.env.NEXT_PUBLIC_URL}/dashboard`,
+            })
+          }
+        />
+        <GoogleLoginButton
+          onClick={() =>
+            signIn(providers.google.id, {
+              callbackUrl: `${process.env.NEXT_PUBLIC_URL}/dashboard`,
+            })
+          }
+        />
+        <EmailLoginButton
           onClick={() =>
             signIn(null, {
               callbackUrl: `${process.env.NEXT_PUBLIC_URL}/dashboard`,
             })
           }
-          title={"Log In / Sign Up"}
         />
       </div>
     </div>
@@ -25,8 +49,9 @@ export default function Home() {
 
 export async function getServerSideProps(ctx) {
   const session = await getSession(ctx);
+  const providers = await getProviders();
 
   return {
-    props: {},
+    props: { providers },
   };
 }
